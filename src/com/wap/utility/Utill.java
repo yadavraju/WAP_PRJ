296fbe90a477f2c8ce88cde60aefd741335cf35f
yadavraju <razuyadav93@gmail.com>
2017-05-20 16:31:52 -0500

    utility class only

--- src/com/wap/utility/Utill.java
+++ src/com/wap/utility/Utill.java
@@ -4,7 +4,7 @@ public class Utill {
 	
 	public static boolean suceesfullyRegister = false;
 	
-	public static String successJson = "{\"code\": 200,\"msg\": \"sucess fully done\"}";
-	public static String failJson = "{\"code\": 201,\"msg\": \"fail\"}";
+	public static String successJson = "{\"code\": \"200\",\"msg\": \"sucess fully done\"}";
+	public static String failJson = "{\"code\": \"201\",\"msg\": \"fail\"}";
 
 }
